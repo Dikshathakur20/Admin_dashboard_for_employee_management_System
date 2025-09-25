f9377dbf08792f04c18791abd15b85696af004b5
Dikshathakur20 <ranadikshathakur20@gmail.com>
2025-09-25 15:50:26 +0530

    Update EditDesignationDialog.tsx

--- src/components/dashboard/EditDesignationDialog.tsx
+++ src/components/dashboard/EditDesignationDialog.tsx
@@ -113,7 +113,9 @@ export const EditDesignationDialog = ({
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="sm:max-w-[400px] bg-white text-black rounded-xl shadow-lg border border-gray-200">
+      <DialogContent className="sm:max-w-[400px] bg-white text-black rounded-xl shadow-lg border border-gray-200"style={{
+            background: "linear-gradient(-45deg, #ffffff, #c9d0fb)",
+          }}>
         <DialogHeader>
           <DialogTitle>Edit Designation</DialogTitle>
           
