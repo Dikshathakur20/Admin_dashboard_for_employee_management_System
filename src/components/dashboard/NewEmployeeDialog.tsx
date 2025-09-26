0b333294b852635497f88ea4e6c3d6a99beded09
Dikshathakur20 <ranadikshathakur20@gmail.com>
2025-09-26 14:43:20 +0530

    Update NewEmployeeDialog.tsx

--- src/components/dashboard/NewEmployeeDialog.tsx
+++ src/components/dashboard/NewEmployeeDialog.tsx
@@ -172,7 +172,7 @@ export const NewEmployeeDialog = ({ open, onOpenChange, onEmployeeAdded }: NewEm
                 value={firstName}
                 maxLength={25}
                  className="border border-blue-500 focus:ring-2 focus:ring-blue-600 focus:border-blue-600 bg-blue-50 text-blue-900 placeholder-blue-400 rounded-md"
-                onChange={e => /^[A-Za-z ]*$/.test(e.target.value) && setFirstName(capitalizeWords(e.target.value))}
+                onChange={e => /^[A-Za-z\s]*$/.test(e.target.value) && setFirstName(capitalizeWords(e.target.value))}
                 onPaste={e => e.preventDefault()}
                 required
               />
@@ -184,7 +184,7 @@ export const NewEmployeeDialog = ({ open, onOpenChange, onEmployeeAdded }: NewEm
                 value={lastName}
                 maxLength={25}
                  className="border border-blue-500 focus:ring-2 focus:ring-blue-600 focus:border-blue-600 bg-blue-50 text-blue-900 placeholder-blue-400 rounded-md"
-                onChange={e => /^[A-Za-z ]*$/.test(e.target.value) && setLastName(capitalizeWords(e.target.value))}
+                onChange={e => /^[A-Za-z\s]*$/.test(e.target.value) && setLastName(capitalizeWords(e.target.value))}
                 onPaste={e => e.preventDefault()}
                 required
               />
