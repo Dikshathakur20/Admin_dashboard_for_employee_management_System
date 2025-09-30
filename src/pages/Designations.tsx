3aa685d1ea33a556ef63f9a8ed172f192437896b
Dikshathakur20 <ranadikshathakur20@gmail.com>
2025-09-30 16:43:45 +0530

    Update Designations.tsx

--- src/pages/Designations.tsx
+++ src/pages/Designations.tsx
@@ -161,7 +161,11 @@ const Designations = () => {
           {/* Header with Title + Search + Actions */}
           <CardHeader className="px-0 py-2">
             <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-2">
-              <CardTitle className="text-2xl font-bold">Designations</CardTitle>
+             <CardTitle className="text-2xl font-bold">
+  {departmentFilter
+    ? `Designation : ${getDepartmentName(Number(departmentFilter))} Department`
+    : "Designations"}
+</CardTitle>
 
               <div className="flex flex-col sm:flex-row sm:items-center gap-2 w-full md:w-auto">
                 {/* Search bar */}
