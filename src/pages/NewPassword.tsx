1b19e671025b56a3ee9e5cd9c057db0fb92d5cb7
Dikshathakur20 <ranadikshathakur20@gmail.com>
2025-10-07 16:10:44 +0530

    Update NewPassword.tsx

--- src/pages/NewPassword.tsx
+++ src/pages/NewPassword.tsx
@@ -99,13 +99,13 @@ const NewPassword = () => {
                 onChange={(e) => setPassword(e.target.value)}
                 required
               />
-              <button
+              <span
                 type="button"
                 onClick={() => setShowPassword(!showPassword)}
                 className="absolute right-3 top-9 text-gray-500 hover:text-gray-700"
               >
                 {showPassword ? <EyeOff size={6} /> : <Eye size={6} />}
-              </button>
+              </span>
             </div>
 
             {/* Confirm Password Field with Eye Toggle */}
@@ -119,13 +119,13 @@ const NewPassword = () => {
                 onChange={(e) => setConfirmPassword(e.target.value)}
                 required
               />
-              <button
+              <span
                 type="button"
                 onClick={() => setShowConfirmPassword(!showConfirmPassword)}
                 className="absolute right-3 top-9 text-gray-500 hover:text-gray-700"
               >
                 {showConfirmPassword ? <EyeOff size={6} /> : <Eye size={6} />}
-              </button>
+              </span>
             </div>
 
             <Button
